3b31bd24ff6eba2cef35da1e9f890b94bcb8cf52
Syrine <sys75@orange.fr>
2022-02-17 23:18:13 +0100

    ajout du controle de mdp

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,11 +2,13 @@
 const express = require('express');
 // creer un routeur
 const router = express.Router();
+// validation mot de passe
+const password = require('../middleware/password')
 // import du controller du user
 const userCtrl = require('../controllers/user');
 
 // routes pour user
-router.post('/signup', userCtrl.signup);
+router.post('/signup', password, userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 // exporter ce router
